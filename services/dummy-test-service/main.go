a1dd14950426f2a510415dfed5c99dab83ae1660
Bernardo <bernardomiguel0@gmail.com>
2022-11-08 12:56:35 +0000

    Input Validation fully implemented regarding URL parsing

--- services/dummy-test-service/main.go
+++ services/dummy-test-service/main.go
@@ -62,31 +62,18 @@ func (s *serverHandler) handleUser(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 	url := strings.Split(path, "/")
 
-	users := url[1]
-
 	mapdb := s.db.getUserList()
 
-	if users != "users" {
-		io.WriteString(w, "Looks like you are trying to access an inexistent endpoint.\n")
+	flag := URLValidation(url, mapdb, w)
+
+	if flag == 1 {
+		jsonResp, err := json.Marshal(s.db.getUser(url[2]))
+		if err != nil {
+			log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+		}
+		w.Write(jsonResp)
 	} else {
-		flag := 0
-
-		for i := 0; i < len(mapdb["Users"]); i++ {
-			if url[2] == mapdb["Users"][i].Username {
-				flag = 1
-				break
-			}
-		}
-
-		if flag == 1 {
-			jsonResp, err := json.Marshal(s.db.getUser(url[2]))
-			if err != nil {
-				log.Fatalf("Error happened in JSON marshal. Err: %s", err)
-			}
-			w.Write(jsonResp)
-		} else {
-			io.WriteString(w, "Looks like you are trying to access an inexistent endpoint.\n")
-		}
+		io.WriteString(w, "Looks like you are trying to access an inexistent endpoint.\n")
 	}
 }
 
@@ -98,41 +85,23 @@ func (s *serverHandler) handleCity(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 	url := strings.Split(path, "/")
 
-	users := url[1]
-
 	mapdb := s.db.getUserList()
 
-	if users != "users" {
-		io.WriteString(w, "Looks like you are trying to access an inexistent endpoint.\n")
-		log.Fatalf("Error! Trying to access an inexistent endpoint.\n")
-	} else {
+	flag := URLValidation(url, mapdb, w)
+
+	if flag == 1 {
 		url3, err1 := strconv.Atoi(url[3])
 		if err1 != nil {
 			log.Fatalf("Error happened in Atoi Err: %s", err1)
 		}
 
-		flag := 0
-
-		for i := 0; i < len(mapdb["Users"]); i++ {
-			if url[2] == mapdb["Users"][i].Username {
-				for j := 0; j < len(mapdb["Users"][i].City); j++ {
-					if url3 == mapdb["Users"][i].City[j].CityID {
-						flag = 1
-						break
-					}
-				}
-			}
+		jsonResp, err := json.Marshal(s.db.getCity(url[2], url3))
+		if err != nil {
+			log.Fatalf("Error happened in JSON marshal. Err: %s", err)
 		}
-
-		if flag == 1 {
-			jsonResp, err := json.Marshal(s.db.getCity(url[2], url3))
-			if err != nil {
-				log.Fatalf("Error happened in JSON marshal. Err: %s", err)
-			}
-			w.Write(jsonResp)
-		} else {
-			io.WriteString(w, "Looks like you are trying to access an inexistent endpoint.\n")
-		}
+		w.Write(jsonResp)
+	} else {
+		io.WriteString(w, "Looks like you are trying to access an inexistent endpoint.\n")
 	}
 }
 
@@ -144,14 +113,11 @@ func (s *serverHandler) handleBuilding(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 	url := strings.Split(path, "/")
 
-	users := url[1]
-
 	mapdb := s.db.getUserList()
 
-	if users != "users" {
-		io.WriteString(w, "Looks like you are trying to access an inexistent endpoint.\n")
-		log.Fatalf("Error! Trying to access an inexistent endpoint.\n")
-	} else {
+	flag := URLValidation(url, mapdb, w)
+
+	if flag == 1 {
 		url3, err1 := strconv.Atoi(url[3])
 		if err1 != nil {
 			log.Fatalf("Error happened in Atoi Err: %s", err1)
@@ -162,33 +128,105 @@ func (s *serverHandler) handleBuilding(w http.ResponseWriter, r *http.Request) {
 			log.Fatalf("Error happened in Atoi Err: %s", err2)
 		}
 
-		flag := 0
+		jsonResp, err := json.Marshal(s.db.getBuilding(url[2], url3, url4))
+		if err != nil {
+			log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+		}
+		w.Write(jsonResp)
+	} else {
+		io.WriteString(w, "Looks like you are trying to access an inexistent endpoint.\n")
+	}
+}
 
-		for i := 0; i < len(mapdb["Users"]); i++ {
-			if url[2] == mapdb["Users"][i].Username {
-				for j := 0; j < len(mapdb["Users"][i].City); j++ {
-					if url3 == mapdb["Users"][i].City[j].CityID {
-						for k := 0; k < len(mapdb["Users"][i].City[j].Buildings); k++ {
-							if url4 == mapdb["Users"][i].City[j].Buildings[k].BuildingID {
+func URLValidation(url []string, mapdb map[string][]user, w http.ResponseWriter) int {
+
+	flag := 0
+
+	users := url[1]
+
+	switch {
+	case len(url) == 4:
+		{
+			if users != "users" {
+				io.WriteString(w, "Looks like you are trying to access an inexistent endpoint.\n")
+			} else {
+
+				for i := 0; i < len(mapdb["Users"]); i++ {
+					if url[2] == mapdb["Users"][i].Username {
+						flag = 1
+						break
+					}
+				}
+			}
+		}
+	case len(url) == 5:
+		{
+			if users != "users" {
+				io.WriteString(w, "Looks like you are trying to access an inexistent endpoint.\n")
+				log.Fatalf("Error! Trying to access an inexistent endpoint.\n")
+			} else {
+				url3, err1 := strconv.Atoi(url[3])
+				if err1 != nil {
+					log.Fatalf("Error happened in Atoi Err: %s", err1)
+				}
+
+				for i := 0; i < len(mapdb["Users"]); i++ {
+					if url[2] == mapdb["Users"][i].Username {
+						for j := 0; j < len(mapdb["Users"][i].City); j++ {
+							if url3 == mapdb["Users"][i].City[j].CityID {
 								flag = 1
 								break
 							}
 						}
 					}
+					if flag == 1 {
+						break
+					}
 				}
 			}
 		}
+	case len(url) == 6:
+		{
+			if users != "users" {
+				io.WriteString(w, "Looks like you are trying to access an inexistent endpoint.\n")
+				log.Fatalf("Error! Trying to access an inexistent endpoint.\n")
+			} else {
+				url3, err1 := strconv.Atoi(url[3])
+				if err1 != nil {
+					log.Fatalf("Error happened in Atoi Err: %s", err1)
+				}
 
-		if flag == 1 {
-			jsonResp, err := json.Marshal(s.db.getBuilding(url[2], url3, url4))
-			if err != nil {
-				log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+				url4, err2 := strconv.Atoi(url[4])
+				if err2 != nil {
+					log.Fatalf("Error happened in Atoi Err: %s", err2)
+				}
+
+				for i := 0; i < len(mapdb["Users"]); i++ {
+					if url[2] == mapdb["Users"][i].Username {
+						for j := 0; j < len(mapdb["Users"][i].City); j++ {
+							if url3 == mapdb["Users"][i].City[j].CityID {
+								for k := 0; k < len(mapdb["Users"][i].City[j].Buildings); k++ {
+									if url4 == mapdb["Users"][i].City[j].Buildings[k].BuildingID {
+										flag = 1
+										break
+									}
+								}
+							}
+							if flag == 1 {
+								break
+							}
+						}
+					}
+					if flag == 1 {
+						break
+					}
+				}
 			}
-			w.Write(jsonResp)
-		} else {
-			io.WriteString(w, "Looks like you are trying to access an inexistent endpoint.\n")
 		}
+	default:
+		break
 	}
+	return flag
 }
 
 func (s *serverHandler) init_db() mockDB {
@@ -201,7 +239,7 @@ func (s *serverHandler) init_db() mockDB {
 
 func (s *serverHandler) handleRoot(w http.ResponseWriter, r *http.Request) {
 
-	w.Write([]byte("Welcome to Stickian!\n"))
+	io.WriteString(w, "Welcome to Stickian!\n")
 }
 
 func main() {
